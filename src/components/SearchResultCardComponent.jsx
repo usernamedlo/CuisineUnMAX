ae2ced7b584d76b0fb1cadce1921eab3c593e2bd
Loïc Ghijselings <l.ghijselings@student.helmo.be>
2023-04-04 22:24:20 +0200

    🚧 WIP

--- src/components/SearchResultCardComponent.jsx
+++ src/components/SearchResultCardComponent.jsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 function SearchResultCardComponent({ recipe }) {
   const [isZoomed, setIsZoomed] = useState(false);
   const [showDescription, setShowDescription] = useState(false);
+  const [showCircles, setShowCircles] = useState(false);
 
   return (
     <div
@@ -10,10 +11,12 @@ function SearchResultCardComponent({ recipe }) {
       onMouseEnter={() => {
         setIsZoomed(true);
         setShowDescription(true);
+        setShowCircles(true);
       }}
       onMouseLeave={() => {
         setIsZoomed(false);
         setShowDescription(false);
+        setShowCircles(false);
       }}
     >
       <img
@@ -27,11 +30,11 @@ function SearchResultCardComponent({ recipe }) {
           transition: "transform 0.4s ease",
         }}
       />
-      
+
       <div
-        className="h-12 w-12 m-2 rounded-full border-4 border-red-500 bg-red-500 flex justify-center items-center text-center"
+        className="h-12 w-12 m-2 rounded-full border-4 border-red-500 bg-red-500 flex justify-center items-center"
         style={{
-          transform: showDescription ? "translateX(10%)" : "translateX(0)",
+          transform: showCircles ? "translateX(10%)" : "translateX(-80%)",
           transition: "transform 0.4s ease",
         }}
       >
@@ -39,9 +42,9 @@ function SearchResultCardComponent({ recipe }) {
       </div>
 
       <div
-        className="h-12 w-12 m-2 rounded-full border-4 border-blue-900 bg-blue-500 flex justify-center items-center text-center"
+        className="h-12 w-12 m-2 rounded-full border-4 border-blue-900 bg-blue-500 flex justify-center items-center"
         style={{
-          transform: showDescription ? "translateX(10%)" : "translateX(0)",
+          transform: showCircles ? "translateX(10%)" : "translateX(-80%)",
           transition: "transform 0.4s ease",
         }}
       >
@@ -68,16 +71,16 @@ function SearchResultCardComponent({ recipe }) {
             {recipe.nom}
           </h3>
           {/* <div
-            className="h-full w-full"
-            style={{
-              opacity: showDescription ? 1 : 0,
-              transition: "opacity 0.4s ease",
-            }}
-          >
-            <p className="h-24 text-center text-basic text-gray-200 bg-black">
-              {recipe.description}
-            </p>
-          </div> */}
+                className="h-full w-full"
+                style={{
+                opacity: showDescription ? 1 : 0,
+                transition: "opacity 0.4s ease",
+                }}
+                >
+                <p className="h-24 text-center text-basic text-gray-200 bg-black">
+                {recipe.description}
+                </p>
+                </div> */}
         </div>
       </div>
     </div>
@@ -86,30 +89,4 @@ function SearchResultCardComponent({ recipe }) {
 
 export default SearchResultCardComponent;
 
-{
-  /*    <div
-      className="h-[32rem] w-[19rem] relative overflow-hidden rounded-lg"
-      onMouseEnter={() => setIsZoomed(true)}
-      onMouseLeave={() => setIsZoomed(false)}
-    >
-      <img
-        src={require(`../data/${recipe.img}`)}
-        alt={recipe}
-        className={`h-full w-full rounded-lg object-cover ${
-          isZoomed ? "absolute" : ""
-        }`}
-        style={{
-          transform: isZoomed ? "scale(1.1)" : "scale(1)",
-          transition: "transform 0.4s ease",
-        }}
-      />
-      <div
-        className="absolute bottom-0 left-0 w-full p-4"
-        style={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}
-      >
-        <h3 className="text-2xl font-medium text-white">{recipe.nom}</h3>
-        <p className="text-sm text-gray-200">{recipe.description}</p>
-      </div>
-    </div>*/
-  // https://codepen.io/mrtrimble/pen/RwPazwg
-}
+// https://codepen.io/mrtrimble/pen/RwPazwg
